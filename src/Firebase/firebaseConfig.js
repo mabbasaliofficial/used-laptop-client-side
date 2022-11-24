a60804c7fc8085977c469dd47903f1640d8c48b3
mabbasaliofficial <mabbasaliofficial@gmail.com>
2022-11-24 18:47:13 +0600

    firebase added

---
+++ src/Firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAcY3by7F6fJqysJ57byoiOhDbYMYJI_mU",
+  authDomain: "laptop-showcase.firebaseapp.com",
+  projectId: "laptop-showcase",
+  storageBucket: "laptop-showcase.appspot.com",
+  messagingSenderId: "812326571797",
+  appId: "1:812326571797:web:70255b8a7e515e1a10e31c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
